0a3036d04f5ded0bb2d2725da131956b5635c5cb
abel724 <albertobelforte@virgilio.it>
2026-08-20 08:07:37 +0200

    Miglioramento campo ricerca

--- dati.js
+++ dati.js
@@ -8,3 +8,51 @@ for (let i = 1; i <= 75; i++) {
         persone: []
     };
 }
+
+// DATI DI PROVA
+
+datiFoto["F001"] = {
+    evento: "Raduno ORGonauti presso Auchan del 18/12/2025",
+    persone: ["Enzo Antonacci", "Mariella Tangorra"]
+};
+
+datiFoto["F002"] = {
+    evento: "Centro meccanografico siderurgico",
+    persone: ["Enzo Loperfido", "Raffaele Cocchiaro"]
+};
+
+datiFoto["F008"] = {
+    evento: "Centro meccanografico siderurgico",
+    persone: [
+        "Cesarin Gervasi",
+        "Sebastiano Gonnella",
+        "Piero Spedicati",
+        "Antonio Violino",
+        "Vito Calamita",
+        "Peppe Axo",
+        "Lino Battista",
+        "Pasquale Ferrara",
+        "Antonio Candelli"
+    ]
+};
+
+datiFoto["F009"] = {
+    evento: "Evento sconosciuto",
+    persone: [
+        "Donato Sofia",
+        "Enzo Picheca",
+        "Enzo De Leonardis",
+        "Remo Epifani"
+    ]
+};
+
+datiFoto["F017"] = {
+    evento: "Raduno ORGonauti presso Auchan del 18/12/2025",
+    persone: [
+        "Alberto Belforte",
+        "Vittorio Ritelli",
+        "Ciccio Di Maggio",
+        "Mariapia De Marzo",
+        "Rosa Lodeserto"
+    ]
+};
