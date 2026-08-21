5496a2f5b6cb391cb429beeac9d32d98238bacf0
abel724 <albertobelforte@virgilio.it>
2026-08-21 21:02:19 +0200

    Aggiornamento foto F012

--- dati.js
+++ dati.js
@@ -109,7 +109,7 @@ datiFoto["F012"] = {
         "Mimmo Pignatelli",
         "Stefano Bellucco",
         "Orazio Meo",
-        "Enzo e"Pippo Pasquino", "e altri"        
+        "Enzo e"Pippo Pasquino", "altri..."        
     ]
 };
 datiFoto["F017"] = {
